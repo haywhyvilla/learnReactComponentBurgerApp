efef0eff7c67e0095c3b4737ec3e936a0f38f519
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-02-26 20:04:02 +0100

    done

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Aux1 from "../../hoc/Aux1";
+import classes from "./Layout.css";
 
 const Layout = (props) => (
   <Aux1>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux1>
 );
 
