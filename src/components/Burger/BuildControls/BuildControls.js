576e318b5b44132ecbdedd0b1bd368b09dd6c603
Olatunji Ayobami Samson <114535570+haywhyvilla@users.noreply.github.com>
2023-02-27 23:25:33 +0100

    build

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,5 +1,20 @@
 import React from "react";
 
-const BuildControls = (props) => <div></div>;
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Meat", type: "meat" },
+];
+
+const BuildControls = (props) => (
+  <div className="BuildControls">
+    {controls.map((ctrl) => (
+      <BuildControl key={ctrl.label} label={ctrl.label} />
+    ))}
+  </div>
+);
 
 export default BuildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import Aux1 from "../../hoc/Aux1";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
   // constructor(props) {
@@ -21,7 +22,7 @@ class BurgerBuilder extends Component {
     return (
       <Aux1>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </Aux1>
     );
   }
